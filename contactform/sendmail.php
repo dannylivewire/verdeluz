bf7b68bd7beaab702cb58781a753bb63656d6952
Daniel Gonzalez <daniel.gonzalez@grainger.com>
2018-12-31 16:25:58 -0600

    adding sendmail for contact form

---
+++ contactform/sendmail.php
@@ -0,0 +1,14 @@
+<?php
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $visitor_email = $_POST['email'];
+        $message = $_POST['message'];
+        $subject = $_POST['subject'];
+        $email_body = "You have received a new message from the user $name.\n". "Here is the message:\n $message". 
+        $company_email = 'info@VerdeLuz.Eco';   
+        $headers = "From: $company_email \r\n";
+        $headers .= "Reply-To: $visitor_email \r\n";
+        $to = "danny.livewire@gmail.com, tonyfcohen@gmail.com, chicagomultipass@gmail.com";
+        mail($to, $subject, $email_body, $headers);
+    }   
+?>
