21fde621604cbcc47cb688bafac7073170467224
Daniel Gonzalez <daniel.gonzalez@bounteous.com>
2020-05-12 20:17:32 -0500

    setup child theme for VerdeLuz

---
+++ blog/wp-content/themes/wp-bootstrap-starter-child-verdeluz/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+// Include php files
+include get_theme_file_path('/includes/shortcodes.php');
+
+// Enqueue needed scripts
+function needed_styles_and_scripts_enqueue() {
+    
+    // Add-ons
+
+    
+    // Custom script
+    wp_enqueue_script( 'wpbs-custom-script', get_stylesheet_directory_uri() . '/assets/javascript/script.js' , array( 'jquery' ) );
+
+    // enqueue style
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+
+}
+add_action( 'wp_enqueue_scripts', 'needed_styles_and_scripts_enqueue' );
+
+function cc_mime_types($mimes) {
+$mimes['svg'] = 'image/svg+xml';
+return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+
+add_filter( 'widget_text', 'do_shortcode' );
+
+//Dynamic Year
+function site_year(){
+	ob_start();
+	echo date( 'Y' );
+	$output = ob_get_clean();
+    return $output;
+}
+add_shortcode( 'site_year', 'site_year' );
+
+//
+// Your code goes below
+//
